1c51657f8394547ecb28d7f89a6316cf0df0a188
daniilkoversun <koversundanil@gmail.com>
2023-09-10 18:19:37 +0300

    Changes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\CarController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,10 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/test', function () {
-    return \App\Models\Mark::all()->toJson();
-});
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/cars', [CarController::class, 'cars']);
